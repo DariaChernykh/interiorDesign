e7ac5f96198e6cb7ce83b507cda2c98d954e0e06
Winkii <plushka002@gmail.com>
2018-09-08 22:10:08 +0300

    correct gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -4,26 +4,24 @@ const gulp = require('gulp');
 const scss = require('gulp-scss');
 const sourcemaps = require('gulp-sourcemaps');
 const del = require('del');
-const autoprefoxer = require('gulp-autoprefoxer');
+const autoprefixer = require('gulp-autoprefixer');
 
 gulp.task('styles', function() {
   return gulp.src('scss/*.scss')
-    .pipe(autoprefoxer)
+    .pipe(autoprefixer())
     .pipe(sourcemaps.init())
     .pipe(scss())
     .pipe(sourcemaps.write())
     .pipe(gulp.dest('css'))
 });
 
-gulp.task('clean', function () {
+gulp.task('clean', function() {
   return del('css')
 });
 
-gulp.task('default',
-  ['clean', 'styles']
-);
+gulp.task('default', gulp.series('clean','styles'));
 
-gulp.task('watch', function () {
+gulp.task('watch', function() {
   gulp.watch('scss/*.*', gulp.series('styles'));
 });
 
